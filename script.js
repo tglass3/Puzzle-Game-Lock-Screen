b36014eb76651333a7fff5e1e1e8f0744b442862
tglass3 <120339411+tglass3@users.noreply.github.com>
2023-10-23 22:59:01 +0000

    Final v3?

--- script.js
+++ script.js
@@ -73,9 +73,9 @@ button.addEventListener( 'click' , () => {
 function unlockGood(){
   gsap.timeline()
     .to('#lockArm', 
-            {transformOrigin:'center center', rotate: -20, duration: 2, delay: 0.5 })
+            {transformOrigin:'center center', rotate: -40, duration: 2, delay: 0.5 })
   .to('#keyhole', {opacity: 0, duration: 0.5})
-  .fromTo(".eye", {transformOrigin: 'center center', opacity: 0, scaleY: 0}, {delay: 0.5, opacity: 1, scaleY: 2.25, duration: 2})
+  .fromTo(".eye", {transformOrigin: 'center center', opacity: 0, scaleY: 0}, {opacity: 1, scaleY: 2.25, duration: 1})
   // .to('#good', {visibility: "visible", delay: 1})
 
   const siteChange = setTimeout(changePageGood, 5500);
